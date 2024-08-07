72390cc8c208ca697ef512145ef17684dfb08561
Алексей Бобков <bobkovalex.yaundex.ru@yandex.ru>
2024-08-07 22:31:35 +0300

    Databases

--- Databases/main.py
+++ Databases/main.py
@@ -1,17 +1,24 @@
+# Импорт модуля psycopg2
 from psycopg2 import *
- 
-DB_NAME = "Users"
-DB_USER = "postgres"
-DB_PASS = "asdfvcxz16022011"
-DB_HOST = "localhost"
-DB_PORT = "5432"
- 
-try:
-    conn = connect(database=DB_NAME,
-                            user=DB_USER,
-                            password=DB_PASS,
-                            host=DB_HOST,
-                            port=DB_PORT)
-    print("Database connected successfully")
-except:
-    print("Database not connected successfully")
+# Импорт переменных из файла config_db
+from config_db import host, db_name, db_password, db_user, port
+
+# try:
+    # Подключение к базе данных
+db_conn = connect(
+    host = host,
+    database= db_name,
+    user= db_user,
+    password = db_password,
+    port = port
+)
+
+# except Exception as e:
+#     # Обработка ошибок
+#     print("[INFO] Ошибка при работе с PostgreSQL:", e)
+
+# finally:
+#     # Закрытие соединения с базой данных
+#     if db_conn:
+#         db_conn.close()
+#         print(f"[INFO] Соединение с базой данных '{db_name}' закрыто.")
