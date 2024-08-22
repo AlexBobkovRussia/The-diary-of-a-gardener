1a5ce28ec08283f591cc31d821e05a977c7119fd
Алексей Бобков <bobkovalex.yaundex.ru@yandex.ru>
2024-08-22 22:03:23 +0300

    Formating

--- Computer_application/Back/Computer_app/about_computer/size_of_screen.py
+++ Computer_application/Back/Computer_app/about_computer/size_of_screen.py
@@ -1,13 +1,9 @@
 import sys
 
-sys.path.append('')
-from screeninfo import get_monitors
+sys.path.append('.')
+
+from pyautogui import size
 
 
-def know_the_size_of_screen() -> dict:
-    '''Функция know_the_size_of_screen возвращает словарь, состоящий из трех элементов: 
-    "name": Имя монитора
-    "width": Ширину монитора
-    "height": Высоту монитора'''
-    for monitor in get_monitors():
-        return {'name': monitor.name, 'width': monitor.width, 'height': monitor.height}
+def know_the_size_of_screen() -> tuple[int, int]:
+    return size()
